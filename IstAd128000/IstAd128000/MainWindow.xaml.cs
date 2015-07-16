0792aa2a5e228c02d4c1f5e830fdca6ed4fdb3d0
Artemiy Firsov <artyomfirsov@mail.ru>
2015-07-16 11:42:36 +0600

    hgignore

---
+++ IstAd128000/IstAd128000/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using InstAd128000.Tabs;
+
+namespace InstAd128000
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AnyButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var tag = Convert.ToString((sender as Button).Tag);
+            var tab = (UserControl)Activator.CreateInstance(Type.GetType("InstAd128000.Tabs." + tag + ", InstAd128000"));
+
+            Panel.Children.Add(tab);
+        }
+    }
+}
---
+++ IstAd128000/Logic/Core/WaitTimer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.UI;
+
+namespace Logic.Core
+{
+    class WaitTimer
+    {
+        public WaitTimer(PhantomJSDriver driver)
+        {
+            Driver = driver;
+            Wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        public PhantomJSDriver Driver { get; set; }
+
+        private WebDriverWait Wait { get; set; }
+
+        public IWebElement FindElement(By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeoutInSeconds));
+                return wait.Until(drv => drv.FindElement(by));
+            }
+            return Driver.FindElement(by);
+        }
+    }
+}
