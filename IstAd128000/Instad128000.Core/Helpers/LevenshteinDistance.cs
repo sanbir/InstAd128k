5b83b4abd23ad098131874855f376aaa6e691e55
Artemiy Firsov <artyomfirsov@mail.ru>
2015-08-09 13:48:10 +0600

    done

--- IstAd128000/Instad128000.Core/Helpers/LevenshteinDistance.cs
+++ IstAd128000/Instad128000.Core/Helpers/LevenshteinDistance.cs
@@ -12,7 +12,6 @@ namespace Instad128000.Core.Helpers
         {
             if (string1 == null) throw new ArgumentNullException("string1");
             if (string2 == null) throw new ArgumentNullException("string2");
-            int dif;
             var m = new int[string1.Length + 1, string2.Length + 1];
 
             for (int i = 0; i <= string1.Length; i++) m[i, 0] = i;
@@ -21,7 +20,7 @@ namespace Instad128000.Core.Helpers
             for (int i = 1; i <= string1.Length; i++)
                 for (int j = 1; j <= string2.Length; j++)
                 {
-                    dif = (string1[i - 1] == string2[j - 1]) ? 0 : 1;
+                    int dif = (string1[i - 1] == string2[j - 1]) ? 0 : 1;
 
                     m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1,
                                              m[i, j - 1] + 1),
