108d87be26c3f62fe6fe0109d4371777dfff2950
Artemiy Firsov <artyomfirsov@mail.ru>
2015-08-09 12:00:33 +0600

    Merged Development into default

---
+++ IstAd128000/InstAd128000.SentenceBoilTest/Sentence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstAd128000.SentenceBoilTest
+{
+    public class Sentence
+    {
+        public Sentence(string initialString)
+        {
+            Initial = initialString;
+            PreviousBolied = new List<string>();
+        }
+
+        private string Initial { get; set; }
+
+        private List<string> PreviousBolied { get; set; }
+
+        public void Flush()
+        {
+            PreviousBolied.RemoveRange(0, PreviousBolied.Count);
+        }
+
+    }
+}
---
+++ IstAd128000/IstAd128000/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Instad128000.Core;
+using Instad128000.Core.Common.Interfaces.Data;
+using Instad128000.Core.Common.Interfaces.Data.Services;
+using Instad128000.Core.Common.Interfaces.Services;
+using Instad128000.Core.Common.Logger;
+using Instad128000.Core.Helpers.Selenium;
+using InstAd128000.Services;
+using InstAd128000.SqlLite;
+using Microsoft.Practices.Unity;
+
+namespace InstAd128000
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.DispatcherUnhandledException += OnDispatcherUnhandledException;
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs dispatcherUnhandledExceptionEventArgs)
+        {
+            Driver.Close();
+        }
+
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            IUnityContainer container = new UnityContainer();
+
+            container.RegisterType<IDbContextFactory,DbContextFactory>();
+            container.RegisterType<ILogger,Log4NetLogger>();
+            container.RegisterType(typeof(ICrudService<>), typeof(CrudService<>));
+            container.RegisterType(typeof(IRepository<>), typeof(Repository<>));
+            container.RegisterType<IRequestService, RequestService>();
+
+            var window = container.Resolve<MainWindow>();
+            window.Show();
+        }
+    }
+}
