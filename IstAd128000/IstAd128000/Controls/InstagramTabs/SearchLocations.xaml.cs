20f7580d61df54e39d12406ff6c8a939d6962542
Alexander Biryukov <aleksbiryukov@gmail.com>
2015-08-24 00:55:14 +0600

    add Bing map

---
+++ IstAd128000/IstAd128000/Controls/InstagramTabs/SearchLocations.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using InstAd128000.Properties;
+using Microsoft.Maps.MapControl.WPF;
+
+namespace InstAd128000.Controls.InstagramTabs
+{
+    /// <summary>
+    /// Interaction logic for SearchLocations.xaml
+    /// </summary>
+    public partial class SearchLocations : UserControl
+    {
+        public SearchLocations()
+        {
+            InitializeComponent();
+            MyMap.CredentialsProvider = new ApplicationIdCredentialsProvider(Settings.Default.BingCredentialsProvider);
+        }
+    }
+}
